386b10bfd9bf989b2b17369726548ab8f023286b
Muhammad Usama <musa@technologies.dk>
2023-08-24 13:25:07 +0500

    mobile nav

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,6 +1,8 @@
-import { ChevronDown, Wrench } from 'lucide-react';
+import { ChevronDown, Menu, Wrench, X } from 'lucide-react';
 
 export const Icons = {
   logo: Wrench,
   chevronDown: ChevronDown,
+  close: X,
+  menu: Menu,
 };
--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,10 +1,12 @@
 import { MainNav } from '@/components/layouts/main-nav';
+import { MobileNav } from '@/components/layouts/mobile-nav';
 
 export function SiteHeader() {
   return (
     <header className='sticky top-0 z-40 w-full border-b bg-background'>
       <div className='container flex h-14 items-center'>
         <MainNav />
+        <MobileNav />
       </div>
     </header>
   );
